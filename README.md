88f3a9f2be19a2fe721aefcd9bee9b78e647cc91
Julius Olamide <juliusolajumokecoder@gmail.com>
2021-07-18 16:12:03 +0100

    first commit for jc_javascript_dev_env

---
+++ README.md
@@ -0,0 +1,7 @@
+# JAVASCRIPT STARTER KIT 
+
+This is my first javascript development environment starter kit currently in progress. 
+
+## CORE FEATURES INCLUDE
+
+- 
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.css';
+
+/* eslint-disable no-console */
+
+const devName = "JSCODER";
+console.log('I\'m trying to work on my JS development environment called ' + devName + ' APP');
